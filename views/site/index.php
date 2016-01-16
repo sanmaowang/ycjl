97469475ddf45b70eed3595b914e17634f665df6
Sanmao <sanmao@Sanmao.local>
2016-01-16 23:11:39 +0800

    t

--- views/site/index.php
+++ views/site/index.php
@@ -13,26 +13,26 @@ $this->title = '宜昌交旅 | 首页';
       <ul id="sloganlist">
         <li class="imgOn">
           <div class="slogan-1-left">
-            <img src="img/slogan-1-1.png" alt=""/>
+            <img src="<?= Yii::$app->request->baseUrl;?>img/slogan-1-1.png" alt=""/>
           </div>
           <div class="slogan-1-right">
-            <img src="img/slogan-1-2.png" alt=""/>
+            <img src="<?= Yii::$app->request->baseUrl;?>img/slogan-1-2.png" alt=""/>
           </div>
         </li>
         <li>
           <div class="slogan-2-left">
-            <img src="img/slogan-2-1.png" alt=""/>
+            <img src="<?= Yii::$app->request->baseUrl;?>img/slogan-2-1.png" alt=""/>
           </div>
           <div class="slogan-2-right">
-            <img src="img/slogan-2-2.png" alt=""/>
+            <img src="<?= Yii::$app->request->baseUrl;?>img/slogan-2-2.png" alt=""/>
           </div>
         </li>
         <li>
           <div class="slogan-3-left">
-            <img src="img/slogan-3-1.png" alt=""/>
+            <img src="<?= Yii::$app->request->baseUrl;?>img/slogan-3-1.png" alt=""/>
           </div>
           <div class="slogan-3-right">
-            <img src="img/slogan-3-2.png" alt=""/>
+            <img src="<?= Yii::$app->request->baseUrl;?>img/slogan-3-2.png" alt=""/>
           </div>
         </li>
       </ul>
@@ -47,19 +47,19 @@ $this->title = '宜昌交旅 | 首页';
           <div class="row">
             <div class="col-xs-4">
               <a href="<?= Url::to(['site/page','slug'=>'media-focus'])?>">
-              <div class="links-item"><img src="img/icon-newspaper.png" alt=""/></div>
+              <div class="links-item"><img src="<?= Yii::$app->request->baseUrl;?>img/icon-newspaper.png" alt=""/></div>
               媒体聚焦
               </a>
             </div>
             <div class="col-xs-4">
               <a href="<?= Url::to(['site/page','slug'=>'about'])?>">
-              <div class="links-item"><img src="img/icon-building.png" alt=""/></div>
+              <div class="links-item"><img src="<?= Yii::$app->request->baseUrl;?>img/icon-building.png" alt=""/></div>
               集团简介
               </a>
             </div>
             <div class="col-xs-4">
               <a href="<?= Url::to(['site/page','slug'=>'party-construction'])?>">
-              <div class="links-item"><img src="img/icon-cs.png" alt=""/ style="margin-left:8px;"></div>
+              <div class="links-item"><img src="<?= Yii::$app->request->baseUrl;?>img/icon-cs.png" alt=""/ style="margin-left:8px;"></div>
               党群工作
               </a>
             </div>
@@ -77,7 +77,7 @@ $this->title = '宜昌交旅 | 首页';
         <div class="column-show">
           <h3 class="column-show-title">员工风采<span>STAFF STYLE</span><a href="" class="more">更多</a></h3>
           <div class="column-thumb">
-            <a href="#" class="thumb-img"><img src="img/topic1.jpg" alt=""></a>
+            <a href="#" class="thumb-img"><img src="<?= Yii::$app->request->baseUrl;?>img/topic1.jpg" alt=""></a>
           </div>
         </div>
       </div>
@@ -88,11 +88,12 @@ $this->title = '宜昌交旅 | 首页';
 <?php 
 $this->registerJsFile('@web/js/jquery.backstretch.min.js',['depends' => [\yii\web\JqueryAsset::className()]]);
 $this->registerJs('
+  var base_url = "'.Yii::$app->request->baseUrl.'";
   $(function(){
       $("body").backstretch([
-          "/img/home-slider-1.jpg",
-          "/img/home-slider-2.jpg",
-          "/img/home-slider-3.jpg",
+          base_url+"/img/home-slider-1.jpg",
+          base_url+"/img/home-slider-2.jpg",
+          base_url+"/img/home-slider-3.jpg",
         ], {fade:"normal",duration: 8000});
       $(window).on("backstretch.before", function (e, instance, index) {
         // Do something
