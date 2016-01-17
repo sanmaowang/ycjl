5d613b94c23c24666f36ac58f49c5f17265c4d3a
Sanmao <sanmao@Sanmao.local>
2016-01-18 00:55:37 +0800

    p

--- views/layouts/inner.php
+++ views/layouts/inner.php
@@ -67,11 +67,11 @@ for ($i = 0;$i < count($g_slug);$i++){
 <?= $content ?>
 <div class="footer">
   <div class="container">
-    <div class="support">&copy; 宜昌交通旅游集团 2015 ALL RIGHTS RESERVED</div>
+    <div class="support">&copy; 宜昌交通旅游集团 2016 ALL RIGHTS RESERVED</div>
     <div class="row">
     <div class="links">
       <p class="pull-left">
-        <span class="light">技术支持: 湖北元速科技有限公司</span></p>
+        鄂ICP备08005844号 <span class="light">技术支持：湖北元速科技有限公司</span></p>
       <p class="pull-right"><?= Links::widget();?> </p>
     </div>
     </div>
