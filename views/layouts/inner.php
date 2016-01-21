3692a95254d76793574f0e47a5795c1913ceffd2
Sanmao <sanmao@Sanmao.local>
2016-01-21 13:01:27 +0800

    t

--- views/layouts/inner.php
+++ views/layouts/inner.php
@@ -29,10 +29,10 @@ $g1_slug = array('introduction','about','chairman','struction','develop','subsid
 $g2_slug = array('culture','idea','group_vi','activity','staff');
 $g3_slug=array('news','group_news','industry_news','media_focus');
 $g4_slug=array('business','trafic','travel');
-$g4_slug = array('party','party_construction','board');
+$g4_slug = array('party','party_construction','quntuan','board');
 $g5_slug= array('theme','sanyansanshi');
 $g6_slug = array('public','notice','job');
-$g_slug = array('home','introduction','about','chairman','struction','develop','subsidiary','culture','idea','group_vi','activity','staff','news','group_news','industry_news','media_focus','business','trafic','travel','party','party_construction','board','theme','sanyansanshi','public','notice','job','ycjyjt','ycgjjt','ycsxlydjq','ycqczl');
+$g_slug = array('home','introduction','about','chairman','struction','develop','subsidiary','culture','idea','group_vi','activity','staff','news','group_news','industry_news','media_focus','business','trafic','travel','party','party_construction','quntuan','board','theme','sanyansanshi','public','notice','job','ycjyjt','ycgjjt','ycsxlydjq','ycqczl');
 $inner = '';
 for ($i = 0;$i < count($g_slug);$i++){
   if(strpos($url,$g_slug[$i])){
