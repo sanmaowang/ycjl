d52fc39a40387c3548d3ab6d1b4fdb649fcb1e99
Sanmao <sanmao@Sanmao.local>
2016-01-23 14:15:13 +0800

    t

--- views/site/index.php
+++ views/site/index.php
@@ -21,18 +21,23 @@ $this->title = '宜昌交旅 | 首页';
         </li>
         <li>
           <div class="slogan-2-left">
-            <img src="<?= Yii::$app->request->baseUrl;?>/img/slogan-2-1.png" alt=""/>
+            <img src="<?= Yii::$app->request->baseUrl;?>/img/slogan/slogan-2-1.png" alt=""/>
           </div>
           <div class="slogan-2-right">
-            <img src="<?= Yii::$app->request->baseUrl;?>/img/slogan-2-2.png" alt=""/>
+            <img src="<?= Yii::$app->request->baseUrl;?>/img/slogan/slogan-2-2.png" alt=""/>
           </div>
         </li>
         <li>
           <div class="slogan-3-left">
-            <img src="<?= Yii::$app->request->baseUrl;?>/img/slogan/slogan-3-1.png" alt=""/>
+            <img src="<?= Yii::$app->request->baseUrl;?>/img/slogan/slogan-3-a.png" alt=""/>
           </div>
           <div class="slogan-3-right">
-            <img src="<?= Yii::$app->request->baseUrl;?>/img/slogan/slogan-3-2.png" alt=""/>
+            <img src="<?= Yii::$app->request->baseUrl;?>/img/slogan/slogan-3-b.png" alt=""/>
+          </div>
+        </li>
+        <li>
+          <div class="slogan-4">
+            <img src="<?= Yii::$app->request->baseUrl;?>/img/slogan/slogan-4.png" alt=""/>
           </div>
         </li>
       </ul>
@@ -46,7 +51,7 @@ $this->title = '宜昌交旅 | 首页';
           </h3>
           <div class="row">
             <div class="col-xs-4 quick-link link-focus">
-              <a href="<?= Url::to(['site/page','slug'=>'media-focus'])?>"  target="_blank">
+              <a href="<?= Url::to(['site/page','slug'=>'media_focus'])?>"  target="_blank">
               <div class="links-item"><img src="<?= Yii::$app->request->baseUrl;?>/img/icon-newspaper.png" alt=""/></div>
               媒体聚焦
               </a>
@@ -58,7 +63,7 @@ $this->title = '宜昌交旅 | 首页';
               </a>
             </div>
             <div class="col-xs-4 quick-link link-party">
-              <a href="<?= Url::to(['site/page','slug'=>'party-construction'])?>"  target="_blank">
+              <a href="<?= Url::to(['site/page','slug'=>'party_construction'])?>"  target="_blank">
               <div class="links-item"><img src="<?= Yii::$app->request->baseUrl;?>/img/icon-cs.png" alt=""/></div>
               党群工作
               </a>
@@ -88,19 +93,20 @@ $this->title = '宜昌交旅 | 首页';
 </div>
 
 <?php 
-// $this->registerJsFile('@web/js/jquery.backstretch.min.js',['depends' => [\yii\web\JqueryAsset::className()]]);
-// $this->registerJs('
-//   var base_url = "'.Yii::$app->request->baseUrl.'";
-//   $(function(){
-//       $("body").backstretch([
-//           base_url+"/img/home-slider-1.jpg",
-//           base_url+"/img/home-slider-7.jpg",
-//           base_url+"/img/home-slider-3.jpg",
-//         ], {fade:"normal",duration: 8000});
-//       $(window).on("backstretch.before", function (e, instance, index) {
-//         $("#sloganlist").find("li").removeClass("imgOn").eq(index).addClass("imgOn");
-//       });
-//   })
-//   ', View::POS_END, 'js-slide'
-// );
+$this->registerJsFile('@web/js/jquery.backstretch.min.js',['depends' => [\yii\web\JqueryAsset::className()]]);
+$this->registerJs('
+  var base_url = "'.Yii::$app->request->baseUrl.'";
+  $(function(){
+      $("body").backstretch([
+          base_url+"/img/home-slider-1.jpg",
+          base_url+"/img/home-slider-7.jpg",
+          base_url+"/img/home-slider-3.jpg",
+          base_url+"/img/home-slider-9.jpg",
+        ], {fade:"normal",duration: 3000});
+      $(window).on("backstretch.before", function (e, instance, index) {
+        $("#sloganlist").find("li").removeClass("imgOn").eq(index).addClass("imgOn");
+      });
+  })
+  ', View::POS_END, 'js-slide'
+);
 ?>
