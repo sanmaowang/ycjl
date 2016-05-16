9d619b239021f95743e1025d9dba4ca2f02a7cac
liugenghao <liugenghao@gmail.com>
2016-05-16 18:06:39 +0800

    beian-img

--- views/layouts/home.php
+++ views/layouts/home.php
@@ -42,10 +42,10 @@ HomeAsset::register($this);
     <div class="row">
     <div class="links">
       <p class="pull-left">
-        鄂ICP备16001924号-1 <span class="light">技术支持：湖北元速科技有限公司</span></p>
+        鄂ICP备16001924号-1  鄂公网安备 42050202000049号<span class="light">技术支持：湖北元速科技有限公司</span></p>
 
       <p class="pull-right"><?= Links::widget();?></p>
-        <a class="beian" target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=42050202000049"><img src="<?= Yii::$app->request->baseUrl;?>/img/police.png" alt="" />  鄂公网安备 42050202000049号</a>
+<!--        <a class="beian" target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=42050202000049"><img src="--><?//= Yii::$app->request->baseUrl;?><!--/img/police.png" alt="" />  鄂公网安备 42050202000049号</a>-->
     </div>
     </div>
   </div>
